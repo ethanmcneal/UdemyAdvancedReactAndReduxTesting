9bf170eb4f141889d7958286eb150bb1da0f84c6
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-17 15:24:54 -0600

    comment list test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,8 +2,14 @@ import React from 'react'
 import App from '../App'
 import { shallow } from 'enzyme'
 import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
 
-it('Shows the comment box component', () => {
+it('Shows the CommentBox component', () => {
     const wrapped = shallow(<App />);
     expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('Shows the CommentList Component', () => {
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentList).length).toEqual(1)
 })
