3c0bd686ac444aad985888ed9af195e7b707d04c
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-17 15:21:26 -0600

    first enzyme test implimentation

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import App from '../App'
+import { shallow } from 'enzyme'
+import CommentBox from '../CommentBox'
 
 it('Shows the comment box component', () => {
-    const div = document.createElement('div')
-
-    ReactDOM.render(<App />, div);
-
-    expect(div.innerHTML).toContain('Comment Box')
-
-    ReactDOM.unmountComponentAtNode(div)
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentBox).length).toEqual(1)
 })
