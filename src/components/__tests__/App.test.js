5d34d7708a4d67987a2fb912efeca18a5cedf258
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-21 15:51:48 -0600

    before each

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,12 +4,14 @@ import { shallow } from 'enzyme'
 import CommentBox from '../CommentBox'
 import CommentList from '../CommentList'
 
+beforeEach(() => {
+    const wrapped = shallow(<App />)
+});
+
 it('Shows the CommentBox component', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it('Shows the CommentList Component', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1)
 })
