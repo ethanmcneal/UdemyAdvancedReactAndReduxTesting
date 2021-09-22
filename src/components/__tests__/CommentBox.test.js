8bb0b49e627a0020753879571d012334ccaddd67
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-22 15:42:40 -0600

    comment box textarea value testing

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { mount , unmount} from 'enzyme'
-import CommentBox from '../CommentBox';
+import React from "react";
+import { mount, unmount } from "enzyme";
+import CommentBox from "../CommentBox";
 
-
-let wrapped
+let wrapped;
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
-})
+	wrapped = mount(<CommentBox />);
+});
 
 afterEach(() => {
-    wrapped.unmount()
-})
+	wrapped.unmount();
+});
 
-it('shows a textarea and button', () => {
+it("shows a textarea and button", () => {
+	expect(wrapped.find("textarea").length).toEqual(1);
+	expect(wrapped.find("button").length).toEqual(1);
+});
 
-    expect(wrapped.find('textarea').length).toEqual(1)
-    expect(wrapped.find('button').length).toEqual(1)
-})
+it("has a text area that users can type in", () => {
+	wrapped.find("textarea").simulate("change", {
+		target: {
+			value: "new Comment",
+		},
+	});
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+});
