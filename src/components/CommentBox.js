9b468b90addd2051b66fe133a215e3373df57626
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-22 15:02:16 -0600

    handle submit

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class CommentBox extends React.Component {
-    render () {
-        return (
-            <form>
-                <h4>Add a comment</h4>
-                <textarea />
-                <div>
-                    <button>Submit</button>
-                </div>
-            </form>
-        )
+	state = { comment: "" };
+
+    handleChange = (event) => {
+        this.setState({ comment: event.target.value })
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState({comment: ''})
+    }
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h4>Add a comment</h4>
+				<textarea onChange={this.handleChange} value={this.state.comment}/>
+				<div>
+					<button>Submit</button>
+				</div>
+			</form>
+		);
+	}
 }
-export default CommentBox
+export default CommentBox;
